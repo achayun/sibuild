89f380ba9c6cd28de5f1da480fb1ef23cd2f1d8d
Ami Chayun <achayun@tenstorrent.com>
2026-06-29 11:37:33 +0300

    Fixed a bug where with the git submodule dependency detection

--- submodule.inc.mk
+++ submodule.inc.mk
@@ -2,8 +2,9 @@
 ifeq "$(origin submodule_inc_mk)" "undefined"
 submodule_inc_mk := defined
 
-%/.git: $(shell git rev-parse --show-toplevel)/.gitmodules
+%/.git: $(shell git rev-parse --absolute-git-dir)/index
 	$(info [GIT] $(@D))
 	@git submodule update --recursive --init $(@D)
+	@touch $@
 
 endif # ifeq "$(origin submodule_inc_mk)" "undefined"
